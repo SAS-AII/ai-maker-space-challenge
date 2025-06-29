e5c087f56001f69f882b807cdf9149f22722b1c4
Steve Arce <steve.arce@aii.cr>
2025-06-28 18:23:09 -0600

    fix: remove unused rehypeHighlight import

--- frontend/components/chat/MarkdownRenderer.tsx
+++ frontend/components/chat/MarkdownRenderer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight';
 import { cn } from '@/lib/utils';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
